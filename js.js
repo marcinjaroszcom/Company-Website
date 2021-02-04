04a6106c4a1d2759d02916d0a7135dc452928cd0
marcinjaroszcom <71874517+marcinjaroszcom@users.noreply.github.com>
2021-02-04 21:29:08 +0100

    Company website

---
+++ js.js
@@ -0,0 +1,15 @@
+/*REVEAL*/
+window.sr = ScrollReveal();
+sr.reveal('.logo1', {
+    reset: true,
+    delay: 500,
+    distance: 0
+});
+sr.reveal('.logo2', {
+    reset: true,
+    delay: 800
+});
+sr.reveal('.logo3', {
+    reset: true,
+    delay: 1000
+});
